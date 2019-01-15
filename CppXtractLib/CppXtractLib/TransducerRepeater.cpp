32247e4f64420acefff33309b8a7dbc599fcf3be
FredericGrondines <Frederic.grondines.1@etsmtl.net>
2019-01-15 09:52:55 -0500

    TransducerRepeater.cpp

--- CppXtractLib/CppXtractLib/TransducerRepeater.cpp
+++ CppXtractLib/CppXtractLib/TransducerRepeater.cpp
@@ -1,2 +1,9 @@
 #include "TransducerRepeater.h"
 
+/*Fonction qui va retourner le symbole
+acutuellement traité*/
+
+std::string TransducerRepeater::transduce(symbol_t symbol)
+{
+	return std::string(1,symbol);
+}
