0d888662030eb79c157ecbf86f7a9939c5b000d8
skyrimax <46486903+skyrimax@users.noreply.github.com>
2019-02-27 17:47:08 -0500

    Update main.cpp
    Ajouté les référence de l'icone de l'application

--- CppXtractLib/CppXtract/main.cpp
+++ CppXtractLib/CppXtract/main.cpp
@@ -2,6 +2,9 @@
 #include "cppxtract.h"
 #include <QtWidgets/QApplication>
 
+// Icon made by Freepik from www.flaticon.com
+// Author : https://www.flaticon.com/authors/freepik
+// Icon : https://www.flaticon.com/free-icon/cpp-file-format-symbol_29039#term=cpp&page=1&position=1
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
